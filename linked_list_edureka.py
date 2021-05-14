2a109a73f6610892bcf21b25afb60ee387f8dc86
JeffyMahin <Mahin1996>
2021-05-14 06:10:18 +0600

    Linked List added.

--- linked_list_edureka.py
+++ linked_list_edureka.py
@@ -1,9 +1,45 @@
 class Node:
-    def __init__(self, data):
+    def __init__(self, data=None):
         self.data = data
-        self.next = next
+        self.next = None
+
 
 class LinkedList:
     def __init__(self):
-        self.head = head
+        self.head = None
+
+    def show_data(self):
+        node = self.head
+        if node is None:
+            print("LL is empty")
+        while node is not None:
+            print(node.data)
+            node = node.next
+
+    def add_data(self, new):
+        new_node = Node(new)
+        new_node.next = self.head
+        self.head = new_node
+
+
+
+ll = LinkedList()
+#
+# elem = Node(1)
+# # print(elem.data)
+# # print(elem.next)
+#
+# ll.head = elem
+# ll.show_data()
+#
+#
+# elem2 = Node(2)
+# ll.head.next = elem2
+#
+# ll.show_data()
+
+ll.add_data(1)
+ll.add_data(2)
+ll.add_data(3)
+ll.show_data()
 
